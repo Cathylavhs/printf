67b74ff12c5a2f623e1d4443331e3afce7eadd62
Lorna Ndathe <lornahndathe@gmail.com>
2023-06-27 16:26:18 -0700

    printf

--- functions1.c
+++ functions1.c
@@ -121,10 +121,10 @@ int print_hexa_upper(va_list types, char buffer[],
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @flag_ch: Calculates active flags
- * @width: get width
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
+ * @width: get the width
+ * @precision: Precision specifier
+ * @size: Size specification
+ * Return: amount of chars printed
  */
 int print_hexa(va_list types, char map_to[], char buffer[],
 	int flags, char flag_ch, int width, int precision, int size)
